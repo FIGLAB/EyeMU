4f55fa382c1423cd8763a530fb379da79f71062f
kongmunist <andyking99@gmail.com>
2020-07-16 04:04:48 -0700

    collected a lot of data, trained a few good models

--- flask/static/ML.js
+++ flask/static/ML.js
@@ -1,12 +1,13 @@
 models = [];
+var predictions = [[], []];
 
 function makeModel(){
 
     const model = tf.sequential({
         layers: [
         tf.layers.conv2d({inputShape: [iny, inx, 1], kernelSize: 3, filters: 8, activation: 'relu'}),
-        tf.layers.batchNormalization(),
         tf.layers.maxPooling2d({poolSize: 2}),
+        tf.layers.batchNormalization(),
         tf.layers.flatten(),
         tf.layers.dense({units: 100, activation: 'relu'}),
         tf.layers.dense({units: 2}),
@@ -17,8 +18,37 @@ function makeModel(){
     return model
 }
 
-async function saveModel(){
-    await eyeModel.save('downloads://my-model');
+
+function boostedModel(){
+    const model = tf.sequential({
+        layers: [
+        tf.layers.dense({inputShape: [7], units: 100, activation: 'relu'}),
+        tf.layers.dense({units: 300, activation: 'relu'}),
+        tf.layers.dense({units: 2})
+        ]
+    });
+
+    model.summary();
+    return model
+}
+
+function rescaleIms(eyeImsArray){
+    const shape = [eyeImsArray.length, iny, inx, 1]
+    return tf.tensor(eyeImsArray, shape)
+}
+
+function eyeData2tensor(){
+    console.log('conversion started');
+    tensorEyes = [rescaleIms(eyeData[0]), rescaleIms(eyeData[1])];
+
+    predictions[0] = models[0].predict(tensorEyes[0]).arraySync();
+    predictions[1] = models[1].predict(tensorEyes[1]).arraySync();
+    console.log('conversion done');
+}
+
+// Save and load
+async function saveModel(model){
+    await model.save('downloads://my-model');
 }
 
 
--- flask/static/training.js
+++ flask/static/training.js
@@ -11,6 +11,7 @@ var iny = 25;
 
 // Machine learning things
 var eyeModel;
+var boostModel;
 var lr = .001;
 var epochNums = 50;
 var valsplit = .1;
@@ -51,8 +52,8 @@ async function trainModel(){
     });
 
     // construct x_vect as the left eye ims and y_vect as the screen coordinates
-    aa = tf.tensor(eyeData[0], [eyeData[0].length, iny, inx, 1]);
-//    aa = tf.tensor(eyeData[1], [eyeData[1].length, iny, inx, 1]);
+//    aa = tf.tensor(eyeData[0], [eyeData[0].length, iny, inx, 1]);
+    aa = tf.tensor(eyeData[1], [eyeData[1].length, iny, inx, 1]);
     bb = tf.tensor(eyeVals, [eyeVals.length, 2]);
     epochCount = 0;
 
@@ -74,12 +75,70 @@ async function trainModel(){
      });
 }
 
-async function testModel(){
-    const prediction = eyeModel.predict(tf.tensor(eyeData[0][eyeData[0].length-1], [1, iny, inx, 1]));
-    prediction.print();
-    document.getElementById("prediction").innerHTML = tf.mul(prediction,100).arraySync();
-    myEyesAreUpHere(prediction)
+
+async function trainBoostedModel(){
+    const lr = .002;
+    const epochNums = 100;
+    console.log("training boosted model");
+    document.getElementById("trainingstate").innerHTML = "training boost...";
+
+    // Convert eye data if not already done so
+    if (predictions[0].length == 0){
+        eyeData2tensor();
+    }
+
+    // Init model
+    boostModel = boostedModel();
+
+    // Display the learning rate and set the optimizer
+    console.log(lr);
+    dadam = tf.train.adam(lr);
+
+    // Compile the model
+    boostModel.compile({
+      optimizer: dadam,
+      loss: 'meanSquaredError',
+      metrics: ['mae', 'mse']
+    });
+
+    // x_vect is the left eye pred, right eye pred, head yaw and pitch, and headsize
+    x_vect = predictions[0]
+    if (x_vect[0].length != 7){
+        x_vect.forEach((item, index, arr) =>{
+            arr[index] = arr[index].concat(predictions[1][index], headTilts[index], headSizes[index])
+        });
+        x_vect = tf.tensor(x_vect, [x_vect.length, 7])
+    }
+    console.log(x_vect.shape)
+
+//    aa = tf.tensor(eyeData[1], [eyeData[1].length, iny, inx, 1]);
+    bb = tf.tensor(eyeVals, [eyeVals.length, 2]);
+    epochCount = 0;
+
+    boostModel.fit(x_vect, bb, {
+       epochs: epochNums,
+       batchSize: batchSizeVar,
+       validationSplit: valsplit,
+       callbacks: {
+      onEpochEnd: async (batch, logs) => {
+          console.log(epochCount++, 'Loss: ' + logs.loss.toFixed(5));
+      }
+    }
+    }).then(info => {
+   console.log('Final accuracy', info.history);
+   console.log( info.history['mae']);
+   console.log( info.history['val_mae']);
+    console.log("boost training done");
+    document.getElementById("trainingstate").innerHTML = "boost training done";
+     });
 }
+
+//async function testModel(){
+//    const prediction = eyeModel.predict(tf.tensor(eyeData[0][eyeData[0].length-1], [1, iny, inx, 1]));
+//    prediction.print();
+//    document.getElementById("prediction").innerHTML = tf.mul(prediction,100).arraySync();
+//    myEyesAreUpHere(prediction)
+//}
 
 
 async function main() {
