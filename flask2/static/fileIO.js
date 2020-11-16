0cf13cadae0cd6d7dfa37831f8650c9ed7c62e19
kongmunist <andyking99@gmail.com>
2020-11-15 22:57:58 -0500

    updated download in fileIO as well

--- flask2/static/fileIO.js
+++ flask2/static/fileIO.js
@@ -28,14 +28,10 @@ window.onload = function yea() {
         eyeDataDownloadButton.addEventListener('click', function () {
             console.log('in eyedatadownload click');
             var link = document.createElement('a');
-            link.setAttribute('download', 'gazelData.json');
+            link.target = '_blank';
+            link.download = 'gazelData.json';
             link.href = makeTextFile(getUserDataAsString());
-            document.body.appendChild(link);
-            window.requestAnimationFrame(function () {
-              var event = new MouseEvent('click');
-              link.dispatchEvent(event);
-              document.body.removeChild(link);
-            });
+            link.click();
         }, false);
     }
 }
@@ -46,11 +42,6 @@ function getUserDataAsString(){
         let a = tf.stack(leftEyes_x).arraySync();
         let b = tf.stack(rightEyes_x).arraySync();
         let c = tf.stack(eyeCorners_x).arraySync();
-//        console.log("a", a)
-//        console.log("b", b)
-//        console.log("c", c)
-//        console.log(faceGeom_x)
-//        console.log(screenXYs_y)
 
         return JSON.stringify([a, b, c, faceGeom_x, screenXYs_y]);
     });
@@ -61,38 +52,7 @@ var expose;
 var expose2;
 function openFile(event) {
     var input = event.target;
-    expose2 = input
-
-//    // Read each and append all eye pics
-//    input.files.forEach(function (filename, ind){
-//        const reader = new FileReader();
-//        reader.onload = function(){
-//            loadEyeData=JSON.parse(reader.result);
-//            expose = loadEyeData
-//            console.log(loadEyeData)
-//
-//            // // need to do data cleanup before we wipe the old variables
-////            if (leftEyes_x != undefined){
-////
-////            }
-//
-//            leftEyes_x = tf.unstack(loadEyeData[0])
-//            rightEyes_x = tf.unstack(loadEyeData[1])
-//            eyeCorners_x = tf.unstack(loadEyeData[2])
-//            faceGeom_x = loadEyeData[3]
-//            screenXYs_y = loadEyeData[4]
-//
-//            shuffle(leftEyes_x, rightEyes_x, eyeCorners_x, faceGeom_x, screenXYs_y)
-//
-////            leftEyes_x = leftEyes_x.concat(tf.unstack(loadEyeData[0]))
-////            rightEyes_x = rightEyes_x.concat(tf.unstack(loadEyeData[1]))
-////            eyeCorners_x = eyeCorners_x.concat(tf.unstack(loadEyeData[2]))
-////            faceGeom_x = faceGeom_x.concat(loadEyeData[3])
-////            screenXYs_y = screenXYs_y.concat(loadEyeData[4])
-//        };
-//        reader.readAsText(filename);
-//        console.log("load not done")
-//    });
+    expose2 = input;
 
     // Read each and append all eye pics
     input.files.forEach(function (fileObject, ind){
