2c9743dc5274b93959aa991e3a6bb506fea4da0c
kongmunist <andyking99@gmail.com>
2021-02-05 01:41:49 -0500

    changed all other squares to grey, can't fit text though.

--- flask2/static/neweval.js
+++ flask2/static/neweval.js
@@ -61,10 +61,11 @@ function resetGridColors(){
     for (let div of galleryNumbers){
         div.style.color = "black";
 //        div.style.backgroundColor = divColors[i];
-        div.style.backgroundColor = "grey";
+        galleryElements[i].style.backgroundColor = "grey";
+        i++;
+//        div.style.backgroundColor = "grey";
 //        div.style.innerText = (i%2)*4 + Math.trunc(i/2) + 1;
-//        i++;
-        div.innerText = ""; // Don't show numbers
+        div.innerText = "_"; // Don't show numbers
     }
 }
 
@@ -72,8 +73,10 @@ function setGridColorAndText(square_num, text){
     lookup = [1, 3, 5, 7, 2, 4, 6, 8]
     ind = lookup[square_num-1]
     galleryNumbers[ind-1].style.color = "white";
-    galleryNumbers[ind-1].style.backgroundColor = divColors[i]
-    galleryNumbers[ind-1].innerText = text;
+//    galleryNumbers[ind-1].style.backgroundColor = divColors[i]
+    galleryElements[ind-1].style.backgroundColor = divColors[ind-1];
+//    galleryNumbers[ind-1].innerText = text;
+    galleryNumbers[ind-1].innerText = square_num;
 }
 
 function toggleHide(){
@@ -221,7 +224,16 @@ function startTrial(){
     // Generate which trial is next, display it in trialdisplay
     textElem = document.getElementById("trialdisplay");
     textElem.hidden = false;
+
+    // Figure out which trial is next, check url and then go off the results.
+//    const queryString = window.location.search; // TODO: pre-initialize results1 to 4 with the full list so we can just index in and take them out.
+//    const urlParams = new URLSearchParams(queryString);
+//    if (urlParams.has("trialnum")){
+//        trialNum = parseInt(urlParams.get("trialnum"))
+//    } else{
     trialNum = (getLength('results') + 1) % trialList.length;
+//    }
+
     targetGesture = trialList[trialNum][0];
     targetSquare = trialList[trialNum][1];
 
@@ -318,12 +330,9 @@ function trialEndHandler(detected, target, histories){ // Both in [gestures, seg
     toggleHide();
     textElem = document.getElementById("trialdisplay");
     textElem.hidden = false;
-    textElem.innerHTML = "Trial #" + trialNum + " Over<br><hr>";
+    textElem.innerHTML = "Trial #" + trialNum + " Complete<br><hr><br>Tap to continue";
 
     if (detected[0] == -1){ // If no gesture triggered (timed out)
-//        textElem.innerHTML = "Trial #" + trialNum + " Results<br><hr>";
-//        textElem.innerHTML += "Timed out";
-
         addToStorageArray("results", [Date.now(), [-1, -1], target, histories]);
     } else{
         // Show detected text
@@ -357,22 +366,10 @@ function trialEndHandler(detected, target, histories){ // Both in [gestures, seg
         }
         displayText = gestureNames[detectedGesture];
 
-//        textElem.innerHTML = "Trial #" + trialNum + " Results<br><hr>";
-//        textElem.innerHTML += "Detected gesture and gaze:</h5>"
-//        textElem.innerHTML += "<br>Gesture: " + displayText;
-//        textElem.innerHTML += "<br>Gaze segment: " + segment;
-//
-//        // Show target text
-//        textElem.innerHTML += "<br><br>";
-//        textElem.innerHTML += "Target gesture and gaze:"
-//        textElem.innerHTML += "<br>Gesture: " + gestureNames[target[0]]
-//        textElem.innerHTML += "<br>Gaze segment: " + (target[1]);
-
         // Add to results: [timestamp, detected, target, [gyro history, face dist history, and gaze history]]
         // Goes [gest, segment]          // for target, gest is 0-6 and seg is 0-7. Need to match detected to that
         addToStorageArray("results", [Date.now(), [detectedGesture, segment], target, histories]);
     }
-
 
     trialStarted = false;
 }
